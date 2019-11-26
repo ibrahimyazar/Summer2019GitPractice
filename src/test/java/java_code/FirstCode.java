fce16d9aaf61cf85745ded4c17f6713dde6c9a2c
ibrahim Yazar <ibrahimyazarusa@gmail.com>
2019-11-26 10:46:32 -0500

    Added new class

---
+++ src/test/java/java_code/FirstCode.java
@@ -0,0 +1,10 @@
+package java_code;
+
+public class FirstCode {
+
+    public static void main(String[] args) {
+        int a = 25;
+        int b = 450;
+        System.out.println(a + b);
+    }
+}
